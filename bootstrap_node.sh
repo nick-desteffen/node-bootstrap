076407df3ffca9b8162a2a9f8f8a2dfa7387e877
Nick DeSteffen <nick-desteffen@users.noreply.github.com>
2014-05-20 08:23:23 -0500

    Update to Node v0.10.28

--- bootstrap_node.sh
+++ bootstrap_node.sh
@@ -3,7 +3,7 @@
 apt-get -y -m install build-essential
 
 cd /tmp
-wget http://nodejs.org/dist/v0.10.13/node-v0.10.13.tar.gz
+wget http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz
 tar xvzf node-v0.10.13.tar.gz
 cd node-v0.10.13
 
